b6de5eaf8de68fb1518338deece3c0c9de28755d
rodolfox232 <38224679+rodolfox232@users.noreply.github.com>
2018-04-11 21:49:08 -0500

    3subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,31 +1,33 @@
 // JavaScript Document
 
-$(document).ready(function(e){
-	document.addEventListener("deviceready",function(){
+$(document).ready(function (e) {
+	document.addEventListener("deviceready", function(){
 		$('#disp table td').eq(3).text(device.model);
 		$('#disp table td').eq(5).text(device.cordova);
-	$('#disp table td').eq(7).text(device.platform);	
-	$('#disp table td').eq(9).text(device.version);
-	$('#disp table td').eq(11).text(device.uuid);
-	document.addEventListener("pause",fuction(){ 
-	escribehistoria ('la app se pauso');
-	},false);//pause
-	document.addEventListener("resume",fuction(){ 
-	escribehistoria ('la app se reinició');
-	},false);//resume
-	document.addEventListener("online",fuction(){ 
-	escribehistoria ('la app se conecto a la red');
-	},false);//se conecto
-	document.addEventListener("offline",fuction(){ 
-	escribehistoria ('la app se desconecto de la red');
-	},false);//se desconecto
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
+		document.addEventListener("pause",function(){
+			escribehistoria ('la app se pausó');
+		},false);//pause
+		document.addEventListener("resume", function(){
+			escribehistoria ('la app se reinició');
+		},false); //resume
+		document.addEventListener("online", function() {
+			escribehistoria('la app se conectó a la red');
+		},false);//se conecto
+		document.addEventListener("offline", function() {
+			escribehistoria('la app se desconectó de la red');
+		},false);//se desconecto
+		
+		
+	},false); //ready device
 	
 	
-	},false);//ready device
-	
-
 });//document
 
 function escribehistoria(accion){
 	$('#eHistoria').append('<li>'+accion+'</li>');
 }
+
+
